b322852e4a7be701dac1942efac9ad059679f6d5
Dieguetex <Diegopmansilla@gmail.com>
2018-11-12 21:28:33 +0100

    alfa!

--- src/js/index_new.js
+++ src/js/index_new.js
@@ -1,8 +1,10 @@
 import { Hamburger } from './hamburguer.js';
+import { Main_new } from './main_new.js'
 
 class Index {
     constructor() {
         new Hamburger();
+        new Main_new();
         
     }  
 }
--- src/js/main_new.js
+++ src/js/main_new.js
@@ -1,42 +1,4 @@
-class Hamburger{
-    constructor() {
-        /* Hamburguesa vieja editable
-        this.hamburger = document.querySelector("#hamburger")
-        this.hamburger.addEventListener('click', this.abrirMenu.bind(this))*/
-
-        this.hamburgerDiv = document.querySelector('#icon');
-        this.divs = document.querySelector('#divTop');
-        this.divHead = document.querySelector('.head');
-        this.menuMobile = document.querySelector('#menuMobile');
-
-      
-        this.hamburgerDiv.addEventListener('click', this.abrirMenu.bind(this));
-        document.addEventListener('scroll', this.animationHamburguer.bind(this));
-    }
-    
-
-    abrirMenu() {
-        /* Hamburguesa vieja editable
-        this.hamburger.classList.toggle("is-active"); */
-        this.divs.classList.toggle('open');
-        this.menuMobile.classList.toggle('active');
-
-    }
-
-    animationHamburguer(oE){
-        let pos = oE.target.scrollingElement.scrollTop;
-        
-        if(pos > 0) {
-            this.divHead.classList.add('animate');
-            this.menuMobile.classList.remove('active');
-            this.divs.classList.remove('open');
-        } else {
-            this.divHead.classList.remove('animate');
-        }
-    }
-}
-
-class Main_new {
+export class Main_new {
     constructor(){
         document.addEventListener('scroll', this.animarSkills.bind(this));
         this.nodeBas = document.querySelectorAll('.nodeBas');
@@ -91,13 +53,3 @@ class Main_new {
         return isVisible;
     }
 }
-
-class Index {
-    constructor() {
-        new Hamburger();
-        new Main_new();
-        
-    }  
-}
-
-document.addEventListener('DOMContentLoaded', () => {new Index();});
